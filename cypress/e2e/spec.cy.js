564bdb2e6e5327102ad8658e25b2ec9775fc2a68
devkobby24 <nanakobby2002@gmail.com>
2025-02-11 18:16:55 +0000

    test: update Cypress test descriptions and add external site navigation test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,5 @@
 describe('My First Test', () => {
-  it('Gets, types, and asserts', () => {
+  it('Gets, transitions, and asserts', () => {
     cy.visit('http://localhost:5173/')
 
     // Ensure the "View Details" button exists and click it
@@ -13,5 +13,17 @@ describe('My First Test', () => {
 
     // Verify the URL is back to the homepage
     cy.url().should('eq', 'http://localhost:5173/')
+  })
+})
+
+
+describe('My external site test', () => {
+  it('Visits the external site', () => {
+    cy.visit('https://intabhr.com/')
+
+    cy.contains('About').click()
+
+    // Verify the URL is the external site
+    cy.url().should('eq', 'https://intabhr.com/About')
   })
 })
