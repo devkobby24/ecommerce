8ee302b488ecac01206a369437ade5a543d4ccee
devkobby24 <nanakobby2002@gmail.com>
2025-02-14 19:37:47 +0000

    feat: implement ProductCard component for improved product display in Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,23 +4,20 @@ import {
   Container,
   Typography,
   Grid,
-  Card,
-  CardContent,
-  CardMedia,
   CircularProgress,
   MenuItem,
   Select,
   FormControl,
   InputLabel,
 } from "@mui/material";
-import { Link } from "react-router-dom";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import ProductCard from "../components/productCard";
 
 interface Product {
   id: number;
   title: string;
-  price: number; // Changed to number for sorting
+  price: number;
   category: string;
   image: string;
 }
@@ -166,7 +163,7 @@ const Home = () => {
                 </FormControl>
 
                 {/* Filtering Dropdown */}
-                <FormControl sx={{ minWidth: 150,  mr: { xs: -3, sm: -3 } }}>
+                <FormControl sx={{ minWidth: 150, mr: { xs: -3, sm: -3 } }}>
                   <InputLabel sx={{ mb: 5, fontSize: 16 }} variant="filled">
                     Filter by Category
                   </InputLabel>
@@ -190,79 +187,7 @@ const Home = () => {
           <Grid container spacing={3}>
             {filteredProducts.map((product) => (
               <Grid item xs={12} sm={6} md={4} key={product.id}>
-                <Card sx={{ borderRadius: 2, boxShadow: 5 }}>
-                  <CardMedia
-                    component="img"
-                    height="200"
-                    image={product.image}
-                    alt={product.title}
-                    sx={{ objectFit: "contain", mt: 2 }}
-                  />
-                  <CardContent>
-                    <Typography
-                      variant="h6"
-                      sx={{
-                        fontWeight: "bold",
-                        fontSize: { sm: 12, md: 16, lg: 18 },
-                        display: "-webkit-box",
-                        WebkitLineClamp: 1,
-                        WebkitBoxOrient: "vertical",
-                        overflow: "hidden",
-                        textOverflow: "ellipsis",
-                      }}
-                      gutterBottom
-                    >
-                      {product.title}
-                    </Typography>
-
-                    <Box
-                      display={"flex"}
-                      flexDirection={"row"}
-                      alignItems={"center"}
-                      justifyContent={"space-between"}
-                    >
-                      <Typography
-                        variant="body2"
-                        color="textSecondary"
-                        sx={{ fontSize: { xs: 20, md: 16 } }}
-                      >
-                        {product.category}
-                      </Typography>
-                      <Typography
-                        color="black"
-                        sx={{
-                          fontSize: { xs: 20, md: 16 },
-                          fontWeight: "bold",
-                        }}
-                      >
-                        ${product.price}
-                      </Typography>
-                    </Box>
-
-                    <Box
-                      sx={{
-                        width: { xs: "400px", md: "250px", lg: "400px" },
-                        height: "2px",
-                        backgroundColor: "lightblue",
-                        my: 2,
-                        ml: -2,
-                      }}
-                    />
-
-                    <Link
-                      to={`/product/${product.id}`}
-                      style={{ textDecoration: "none" }}
-                    >
-                      <Button
-                        variant="contained"
-                        color="primary"
-                        sx={{ mt: 1, borderRadius: 5 }}
-                      >
-                        View Details
-                      </Button>
-                    </Link>
-                  </CardContent>
-                </Card>
+                <ProductCard product={product} />
               </Grid>
             ))}
           </Grid>
